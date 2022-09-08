61ff3007a02db599967e021d3f7b1f8afa225b38
Daniel Whalen <daniel.r.whalen@gmail.com>
2022-09-08 15:39:05 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="cyhilbert",
-    version="1.3.0",
+    version="1.3.1",
     author="Danny Whalen",
     author_email="daniel.r.whalen@gmail.com",
     url="https://github.com/invisiblefunnel/cyhilbert",
